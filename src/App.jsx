c98545f2b5e222e615aed6c0ec7c0c808a2649ba
vickygenga <vicky.genga@yahoo.com>
2023-07-04 01:27:38 -0500

    imported a link to a css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
 import './App.css'
+import './path-to-css.css'
 
 function App() {
 const[puppies, setPuppies]=useState(puppyList)
