308304fc53b3607aef3d7ea52ccaa99558e50cef
vickygenga <vicky.genga@yahoo.com>
2023-07-04 01:25:12 -0500

    made several changes to the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
+const[puppies, setPuppies]=useState(puppyList)
+console.log("puppyList: ", puppyList)
+const [featPupId, setFeatPupId] = useState(null)
+function handleClick() {
+  { setFeatPupId(puppy.id)}}
 
-  return (
-  
-      <div>
-      </div>
-    
-  )
-}
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+      
+console.log (featuredPup)
+
+return ( 
+  <div>
+      
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+          </div>
+  </div>
+  )}
+      
 
 export default App
